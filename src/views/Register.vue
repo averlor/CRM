7babc21b9999a0e793b86268c3a8f64f27a4bcc5
Vladimir Abramyants <abramyants@aurum.org>
2019-07-16 18:01:04 +0300

    Added logic for registration

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -95,7 +95,7 @@ export default {
         agree: {checked: v => v}
     },
     methods: {
-        submitHandler() {
+        async submitHandler() {
             if (this.$v.$invalid) {
                 this.$v.$touch()
                 return 
@@ -108,9 +108,11 @@ export default {
                 name: this.name
             }
 
-            console.log(formData)
-
-            this.$router.push('/')
+            try{
+                await this.$store.dispatch('register', formData)
+                this.$router.push('/')
+            }
+            catch(e) {}
 
         }
     }
