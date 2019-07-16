a4b4a406fe55c97878f64c392a56d106abe47c1e
Vladimir Abramyants <abramyants@aurum.org>
2019-07-16 18:00:08 +0300

    Addded new item for technical report

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,3 +1,6 @@
 export default {
-    'logout' : 'Вы покинули систему'
+    'logout' : 'Вы покинули систему',
+    'auth/user-not-found': 'Пользователя с таким email не существует',
+    'auth/wrong-password': 'Не правильный пароль',
+    'auth/email-already-in-use': 'Пользователь с таким email уже существует'
 }
