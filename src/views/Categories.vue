acad76d3ca00aa098052b7731dfc27872d005f7e
Vladimir Abramyants <abramyants@aurum.org>
2019-07-17 17:48:31 +0300

    Added function for update category in parrent

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,10 +4,17 @@
             <h3>Категории</h3>
         </div>
         <section>
-            <div class="row">
+            <Loader v-if="loading" />
+            <div v-else class="row">
                 <CategoryCreate @created="addNewCategory"/>
 
-                <CategoryEdit />
+                <CategoryEdit
+                    v-if="categories.length" 
+                    :categories="categories" 
+                    @updated="updateCategories"
+                    :key="categories.length + updateCount"
+                />
+                <p v-else class="center">Категорий пока нет</p>
             </div>
         </section>
     </div>
@@ -22,7 +29,9 @@ import CategoryEdit from '@/components/CategoryEdit'
 export default {
     name: 'categories',
     data: () => ({
-        categories: []
+        categories: [],
+        loading: true,
+        updateCount: 0
     }),
     components: {
         CategoryCreate,
@@ -31,7 +40,17 @@ export default {
     methods: {
         addNewCategory(category) {
             this.categories.push(category)
+        },
+        updateCategories(category) {
+            const idx = this.categories.findIndex(c => c.id === category.id)
+            this.categories[idx].title = category.title
+            this.categories[idx].limit = category.limit
+            this.updateCount++
         }
+    },
+    async mounted() {
+        this.categories = await this.$store.dispatch('fetchCategories')
+        this.loading = false
     }
 }
 </script>
