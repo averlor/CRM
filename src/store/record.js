d2abefb9aea2a047e2716ef57bfc86aaccb27499
Vladimir Abramyants <abramyants@aurum.org>
2019-07-17 17:50:07 +0300

    created store for record

---
+++ src/store/record.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app'
+
+export default {
+    actions: {
+        async fetchRecords({dispatch, commit}) {
+            try{
+                const uid = await dispatch('getUid')
+                const records = (await firebase.database().ref(`/users/${uid}/records`).once('value')).val() || {}
+                
+                return Object.keys(records).map(key => ({...records[key], id: key}))
+            }
+            catch(e) {
+                commit('SET_ERROR', e)
+                throw e
+            }
+        },
+        async createRecord({dispatch, commit}, record) {
+            try{
+                const uid = await dispatch('getUid')
+                return await firebase.database().ref(`/users/${uid}/records`).push(record)
+            }
+            catch(e) {
+                commit('SET_ERROR', e)
+                throw e
+            }
+        }
+    }
+}
