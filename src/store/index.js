a41dd33c265900e25048712c2d3b6436178314b5
Vladimir Abramyants <abramyants@aurum.org>
2019-07-17 12:25:57 +0300

    Added auth, info, category-modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
+import info from './info'
+import category from './category'
 
 Vue.use(Vuex)
 
@@ -20,9 +22,15 @@ export default new Vuex.Store({
     }
   },
   actions: {
-
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
+      return await res.json()
+    }
   },
   modules: {
-    auth
+    auth,
+    info,
+    category
   }
 })
