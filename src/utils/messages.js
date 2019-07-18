f769a42032f4b4dc66007c82ea5a155b954841c7
Vladimir Abramyants <abramyants@aurum.org>
2019-07-18 16:07:56 +0300

    Added new key for event

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
     'logout' : 'Вы покинули систему',
+    'login':'Войдите в систему, чтобы получить доступ',
     'auth/user-not-found': 'Пользователя с таким email не существует',
     'auth/wrong-password': 'Не правильный пароль',
     'auth/email-already-in-use': 'Пользователь с таким email уже существует'
